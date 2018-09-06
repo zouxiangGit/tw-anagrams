8579cd22eb30d036860d29eef68643b88e3da9dc
Fangxiang Zou <fxzou@thoughtworks.com>
2018-09-06 23:26:42 +0800

    should return six str when str is three character

--- main/main.js
+++ main/main.js
@@ -5,10 +5,18 @@ function anagrams(str) {
     if (str.length === 1) {
         return [str];
     }
-    return [str, reverseString(str)];
+    const charsArray = Array.from(str);
+    return charsArray.reduce((result, char, index) => {
+        anagrams(removeElementAndCopy(charsArray, index))
+            .forEach(shortStr => result.push(char + shortStr));
+        return result;
+    }, []);
 }
 
-function reverseString(str) {
-    return str.split('').reverse().join('');
+function removeElementAndCopy(array, index) {
+    let copy = Array.from(array);
+    copy.splice(index, 1);
+    return copy;
 }
+
 module.exports = anagrams;
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -10,6 +10,10 @@ describe('main()', () => {
     });
 
     it('should return tow str when str is two character', () => {
-        expect(anagrams('ab')).toEqual(['ab','ba']);
+        expect(anagrams('ab')).toEqual(['ab', 'ba']);
+    });
+
+    it('should return six str when str is three character', () => {
+        expect(anagrams('abc')).toEqual(['abc', 'acb', 'bac', 'bca', 'cab', 'cba']);
     });
 });
